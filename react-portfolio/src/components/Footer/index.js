032bb8169a2da904e8e27b4baeeef350abd5749b
Dan Phillips <d.p.phillips@bham.ac.uk>
2020-08-16 21:21:54 +0100

    Footer

---
+++ react-portfolio/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+
+function Footer() {
+    return (        
+            <footer>
+            © Dan Phillips 2020
+            </footer>        
+    )
+}
+export default Footer
